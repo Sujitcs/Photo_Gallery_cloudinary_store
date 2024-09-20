e6285ce5643e41f2af3fb582d61d12f1c2088007
Sujit Mondal <132594980+Sujitcs@users.noreply.github.com>
2024-09-21 00:24:51 +0530

    Update AddPhoto.js

--- frontend/src/pages/AddPhoto.js
+++ frontend/src/pages/AddPhoto.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import '../styles/addphoto.css';
 
-const url='http://localhost:5000';
+const url='https://photo-gallery-cloudinary.onrender.com';
 
 const AddPhoto = () => {
     const [name, setName] = useState('');
