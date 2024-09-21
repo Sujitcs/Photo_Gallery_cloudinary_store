4757394d06fb788a483357e550505ba9d5784511
Sujit Mondal <132594980+Sujitcs@users.noreply.github.com>
2024-09-21 11:55:20 +0530

    Update Base_url.js

--- frontend/src/components/Base_url.js
+++ frontend/src/components/Base_url.js
@@ -1,3 +1,4 @@
 
 // export const url = 'http://localhost:5000';
- export const url='https://photo-gallery-cloudinary.onrender.com';
+// export const url='https://photo-gallery-cloudinary.onrender.com';
+export const url='https://photo-gallery-cloudinary-back.vercel.app/';
